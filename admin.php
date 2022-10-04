a3afd1e952a5c48db9988a995ff44ab8135adae7
unknown <talyssonsilva123@gmail.com>
2022-10-04 15:12:33 -0300

    instrução

---
+++ admin.php
@@ -0,0 +1,42 @@
+
+<html>
+	<head>
+		<title>URNA</title>
+	<!-- CSS only -->
+<link href="./css/bootstrap5.css" rel="stylesheet" >
+<link href="./css/style.css" rel="stylesheet" >
+	</head>
+
+	<body>
+    
+<table align='center' >
+    <tr><td><a link href='./' style="text-decoration:none" >
+        <div class="card text-dark mb-3" style="max-width: 18rem;">
+  <div class="card-header"><b>VOTACAO</b></div>
+  <div class="card-body">
+  <img class='card-img-top' src='img/vote.png' alt='NÃO ENCONTRADO' >
+    <h5 class="card-title"></h5>
+    <p class="card-text"></p>
+  </div>
+</div></a>
+</td>
+<td><a link href='./cadastro.php' style="text-decoration:none"><div class="card text-dark bg-info mb-3" style="max-width: 18rem;">
+  <div class="card-header"><b>CADASTRO DE CANDIDATOS</b></div>
+  <div class="card-body">
+  <img class='card-img-top' src='img/candidato.png' alt='NÃO ENCONTRADO' >
+    <h5 class="card-title"><i class="fa fa-address-book" aria-hidden="true"></i></h5>
+    <p class="card-text"></p>
+  </div>
+</div></a></td><td><a link href='./resultado.php' style="text-decoration:none"><div class="card text-dark bg-success mb-3" style="max-width: 18rem;">
+  <div class="card-header"><b>RESULTADOS</b></div>
+  <div class="card-body">
+  <img class='card-img-top' src='img/resultado.png' alt='NÃO ENCONTRADO' >
+    <h5 class="card-title"></h5>
+    <p class="card-text"></p>
+  </div>
+</div></a></td></tr>
+</table>
+    
+    </body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,20 @@
+Olá, esse projeto é para fins eleitoreis de intranet, utilizando algumas liguagens e suas bibliotecas
+
+as linguagens utilizadas nele são:
+
+1- PHP
+2- JavaScript
+
+coam marcação:
+
+1- HTML
+2- CSS
+
+e o framework web utilizado :
+
+1- Bootstrap 5.0 (https://getbootstrap.com/docs/5.0/getting-started/introduction/)
+
+API:
+
+1- highcharts dashboards (https://www.highcharts.com/)
+
